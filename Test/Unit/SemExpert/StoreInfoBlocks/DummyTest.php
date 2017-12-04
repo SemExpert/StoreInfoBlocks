d8febf7d58ac41e9a05d15d00e038b35b6b20077
Matias Montes <matias@semexpert.com.ar>
2017-12-04 13:13:08 -0300

    Simplified Unit tests to only run module tests

---
+++ Test/Unit/SemExpert/StoreInfoBlocks/DummyTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Barbazul
+ * Date: 29/11/2017
+ * Time: 7:51 PM
+ */
+
+namespace SemExpert\StoreInfoBlocks;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
